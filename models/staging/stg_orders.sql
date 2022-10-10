311b00abbccf90c31f561257d87735626fab459d
poojagauda1705@gmail.com <poojagauda1705@gmail.com>
2022-10-10 18:43:42 +0000

    developing stages

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -3,9 +3,9 @@ select
 o.order_id, o.order_date, o.ship_date, o.ship_mode, o.order_selling_price, o.order_cost_price,
 o.order_selling_price - o.order_cost_price as order_profit,
 --from raw customers
-c.cust_name, c.segment, c.countrty,
+c.cust_id, c.cust_name, c.segment, c.countrty,
 --from raw products
-p.category, p.prod_name, p.sub_category
+p.prod_id, p.category, p.prod_name, p.sub_category
 from {{ ref('raw_orders') }} as o
 left join  {{ ref('raw_customers') }} as c
 on o.cust_id = c.cust_id
